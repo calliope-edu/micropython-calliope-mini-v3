eac9919f190bc252b0e746553e11a89bfb55ca11
Calliope Reorg <juri.wolf@calliope.cc>
2026-06-01 12:59:38 +0200

    micropython v3: radio variant branch (BLE off, MICROBIT_RELEASE 2.1.2b-radio)
    Dedicated branch for the radio build (ships as calliope-v3-radio.hex). BLE stays
    disabled (DEVICE_BLE=1 keeps A+B+Reset pairing); version bump diverges the DAL hash.

--- src/codal_port/mpconfigport.h
+++ src/codal_port/mpconfigport.h
@@ -114,7 +114,7 @@
     { MP_ROM_QSTR(MP_QSTR_open), MP_ROM_PTR(&mp_builtin_open_obj) },
 #endif
 
-#define MICROBIT_RELEASE "2.1.2b"
+#define MICROBIT_RELEASE "2.1.2b-radio"
 #define MICROBIT_BOARD_NAME "Calliope mini"
 #define MICROPY_HW_BOARD_NAME MICROBIT_BOARD_NAME " v" MICROBIT_RELEASE
 #define MICROPY_HW_MCU_NAME "nRF52833"
